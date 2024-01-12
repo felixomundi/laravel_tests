8b89cf95b56299efb895bd33acf3d5a8c4127bff
Felix Omundi <omundifelix30@gmail.com>
2024-01-12 14:13:10 +0300

    first commit

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,17 +2,25 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
-
+use App\Models\User;
+use Tests\TestCase;
 class UserTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $this->assertTrue(true);
+
+    public function test_login_form()
+     {
+        $response = $this->get("/login");
+        $response->assertStatus(200);
+    }
+    public function test_user_duplicates(){
+        $user1 = User::make([
+            "name"=> "Felix",
+            "email"=> "fomundi34@gmail.com",
+        ]);
+        $user2 = User::make([
+            "name"=> "Ka",
+            "email"=> "omundifelix30@gmail.com",
+        ]);
+        $this->assertTrue($user1->name != $user2->name);
     }
 }
